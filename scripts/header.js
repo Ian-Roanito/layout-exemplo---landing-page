9ea4de3cf268c6d8cf86b818dcdc1604cf8ba285
Ian-Roanito <ianroanito027@gmail.com>
2026-05-21 22:06:33 -0300

    Ajustando o menu mobile

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,6 @@
 const header = document.getElementById("header");
 
+
 // Adiciona o a classe rolar no header quando scrolar e tela para baixo
 document.addEventListener("scroll", () => {
 
@@ -16,7 +17,11 @@ const btnMobile = document.getElementById("btnMobile");
 const navList = document.getElementById("navList");
 const link = document.querySelectorAll(".nav-list a");
 
+const body = document.getElementById("body");
+
 btnMobile.addEventListener('click', () => {
+
+    body.classList.toggle('travado');
     
     navList.classList.toggle('display');
 })
@@ -24,7 +29,9 @@ btnMobile.addEventListener('click', () => {
 link.forEach(link => {
 
     link.addEventListener('click', () => {
-    
+        
+        body.classList.remove('travado');
+
         navList.classList.remove('display');
     })
 });
